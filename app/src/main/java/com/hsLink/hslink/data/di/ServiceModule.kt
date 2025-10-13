ac6fdb2823631e2732b925c134ae29bab477f542
Son Juwan <wndhks171@naver.com>
2025-10-13 20:29:22 +0900

    setting/#1: module 추가

---
+++ app/src/main/java/com/hsLink/hslink/data/di/ServiceModule.kt
@@ -0,0 +1,20 @@
+package com.hsLink.hslink.data.di
+
+import com.hsLink.hslink.data.service.DummyService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun providesDummyService(retrofit: Retrofit): DummyService =
+        retrofit.create(DummyService::class.java)
+
+}
