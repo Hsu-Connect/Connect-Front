669cfe1cada25fe0abbe647078ac315301541e54
Son Juwan <wndhks171@naver.com>
2025-10-13 20:28:52 +0900

    setting/#1: core/util, network

---
+++ app/src/main/java/com/hsLink/hslink/core/network/JsonStringExt.kt
@@ -0,0 +1,5 @@
+package com.hsLink.hslink.core.network
+
+fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
+
+fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
