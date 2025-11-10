7197b8d9d6d3afc5a9bcb33098526c345d418c3d
Son Juwan <wndhks171@naver.com>
2025-11-10 20:51:22 +0900

    feat/#18: module 추가 및 수정

---
+++ app/src/main/java/com/hsLink/hslink/data/di/DataSourceModule.kt
@@ -0,0 +1,17 @@
+package com.hsLink.hslink.data.di
+
+import com.hsLink.hslink.data.remote.datasource.PostDataSource
+import com.hsLink.hslink.data.remote.datasourceimpl.PostDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DataSourceModule {
+    @Binds
+    abstract fun bindePostRemoteDataSource(
+        postDataSourceImpl: PostDataSourceImpl,
+    ): PostDataSource
+}
