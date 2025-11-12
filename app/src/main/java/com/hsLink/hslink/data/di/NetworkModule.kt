3696b2e4bb89bd6adc2539be1155e52f4d187491
Son Juwan <wndhks171@naver.com>
2025-11-12 21:38:53 +0900

    feat/#30: module 수정(time out)

--- app/src/main/java/com/hsLink/hslink/data/di/NetworkModule.kt
+++ app/src/main/java/com/hsLink/hslink/data/di/NetworkModule.kt
@@ -15,6 +15,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -25,6 +26,9 @@ object NetworkModule {
     @Singleton
     fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(authInterceptor)
             .addInterceptor(
                 HttpLoggingInterceptor().apply {
